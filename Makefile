a6b412a085aca958e84d9f5d2ae1ce809e052884
brokenFdreams <brokenFdreams@protonmail.com>
2019-04-30 12:52:13 +0300

    change path

--- Makefile
+++ Makefile
@@ -6,30 +6,32 @@
 #    By: fsinged <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/29 12:34:13 by fsinged           #+#    #+#              #
-#    Updated: 2019/04/29 12:45:52 by fsinged          ###   ########.fr        #
+#    Updated: 2019/04/30 12:51:31 by fsinged          ###   ########.fr        #
 #                                                                              #
 #******************************************************************************#
 
-CMD = make -C libft/ make clean -C libft/
-PATH = ./src/
+PATH = ./fillit/
 SRCS = $(PATH)main.c \
 $(PATH)fillit.c \
 $(PATH)check_data.c \
 $(PATH)ft_error.c \
 $(PATH)read_file.c \
-$(PATH)tetri_list.c
+$(PATH)tetri_list.c \
+$(PATH)tetri_map.c \
+./libft/libft.a
 HEADER = ./icludes/fillit.h
 FLAGS = -Wall -Wextra -Werror
 NAME = fillit
 
-all:$(CMD)
+all:$(NAME)
+
+$(NAME):
+	make -C ./libft
 	@gcc $(FLAGS) $(SRCS) -o $(NAME)
 
-cmd:
-	@make -C libft/ make clean -C libft/
-
 clean:
-	@make fclean -C libft/
+	@make clean -C ./libft/
 
 fclean:clean
+	@make fclean -C ./libft/
 	@rm -f $(NAME)
--- fillit/ft_error.c
+++ fillit/ft_error.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   read_file.c                                        :+:      :+:    :+:   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsinged <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/24 14:46:40 by fsinged           #+#    #+#             */
-/*   Updated: 2019/04/26 16:32:03 by fsinged          ###   ########.fr       */
+/*   Created: 2019/04/24 15:08:31 by fsinged           #+#    #+#             */
+/*   Updated: 2019/04/30 12:49:55 by fsinged          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/fillit.h"
+#include "fillit.h"
 
-void	read_file(char *file, char *buf)
+void	ft_error(void)
 {
-	char	c[1];
-	int		ret;
-	int		fd;
-
-	fd = open(file, O_RDONLY);
-	ret = read(fd, buf, BUFF_SIZE);
-	if (ret == -1 || ret > 545)
-		ft_error();
-	buf[ret] = '\0';
-	ret = read(fd, c, 1);
-	if (ret != 0)
-		ft_error();
-	close(fd);
+	ft_putstr("error\n");
+	exit(0);
 }
