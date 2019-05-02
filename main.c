543d0e5cf84e90db47ef2800fe82a6738591de87
Abbie Calandra <acalandr@il-c4.21-school.ru>
2019-05-02 13:15:00 +0300

    clock

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <time.h>
+#include <stdio.h>
 
 int		main(int argc, char **argv)
 {
@@ -27,6 +29,14 @@ int		main(int argc, char **argv)
 	if (!data[0])
 		ft_error();
 	check_data(data, &tlist);
+/*				start time			*/
+	clock_t strt = clock();
+/*									*/
 	fillit(&tlist);
+/*				show diff			*/
+	clock_t endt = clock();
+	double diff = ((double)endt - (double)strt) / CLOCKS_PER_SEC;
+	printf("fillit: %f\n", diff);
+/*				end clock			*/
 	return (0);
 }
